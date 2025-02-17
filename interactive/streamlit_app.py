3954109b8a79876b0cf9b26b69dda1f4f7bb0ded
JJFA-777 <jesunboalade@gmail.com>
2025-02-17 13:10:21 +0000

    streamlit intro framework

--- interactive/streamlit_app.py
+++ interactive/streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write("Hello World")
+st.title("""
+Exploring Exoplanets: Analyzing Exoplanet Data with Python
+""")
+
+st.header("Introduction")
+st.text("""
+Exoplanets, or extrasolar planets, are planets that exist outside our solar system, orbiting distant stars. Since the first confirmed discovery in 1992, thousands of exoplanets have been identified using various detection methods, such as the transit method and radial velocity. These discoveries have revolutionized our understanding of planetary systems, shedding light on the diversity of planets beyond our own.
+
+In this project, we aim to perform Exploratory Data Analysis (EDA) on exoplanets using Python. By leveraging libraries such as pandas, matplotlib, seaborn, and Streamlit, we will analyze an exoplanet dataset to uncover key patterns and relationships. Our goal is to visualize trends, explore correlations between planetary and stellar properties, and gain insights into the characteristics of known exoplanets.
+""")
+
+st.write("## Objectives")
+st.write("""
+- To understand the distribution of exoplanet properties such as mass, radius, and orbital period.
+- To analyze how exoplanets are detected and how discoveries have evolved over time.
+- To explore the relationship between exoplanets and their host stars.
+- To create an interactive Streamlit app that allows users to explore and visualize exoplanet data dynamically.
+""")
+
+
